bc5fb489d3b4491c5bfc9325a5b0ee9a039a9f78
Simon Kenny <digithree@gmail.com>
2016-09-07 11:18:05 +0100

    Add main menu, use as launcher activity

---
+++ app/src/main/java/ie/simonkenny/principleanimationinterpolatortest/MainMenuActivity.java
@@ -0,0 +1,18 @@
+package ie.simonkenny.principleanimationinterpolatortest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by simonkenny on 07/09/2016.
+ */
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main_menu);
+    }
+}
