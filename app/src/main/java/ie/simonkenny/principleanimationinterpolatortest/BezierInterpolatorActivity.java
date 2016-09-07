f5aa0c4666bfaab0758346845cf17e3926ba66e3
Simon Kenny <digithree@gmail.com>
2016-09-07 12:32:20 +0100

    Change colour scheme; add seek bar to bezier curver interpolator test, integrate with EditText fields to edit directly

--- app/src/main/java/ie/simonkenny/principleanimationinterpolatortest/BezierInterpolatorActivity.java
+++ app/src/main/java/ie/simonkenny/principleanimationinterpolatortest/BezierInterpolatorActivity.java
@@ -1,24 +1,26 @@
 package ie.simonkenny.principleanimationinterpolatortest;
 
-import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.SeekBar;
+
+import java.lang.ref.WeakReference;
+import java.util.Locale;
 
 import butterknife.Bind;
-import butterknife.OnClick;
-import ie.simonkenny.principleanimationinterpolatortest.interpolators.BezierInterpolator;
+import butterknife.ButterKnife;
 
 /**
  * Created by simonkenny on 07/09/2016.
  */
 public class BezierInterpolatorActivity extends AppCompatActivity {
 
+    @Bind(R.id.seek_bar_value_edit)
+    AppCompatSeekBar mSbValueEdit;
     @Bind(R.id.edit_text_c1x)
     EditText mEtC1x;
     @Bind(R.id.edit_text_c1y)
@@ -27,16 +29,88 @@ public class BezierInterpolatorActivity extends AppCompatActivity {
     EditText mEtC2x;
     @Bind(R.id.edit_text_c2y)
     EditText mEtC2y;
-    @Bind(R.id.edit_text_duration_ease)
-    EditText mEtDurationEase;
+    @Bind(R.id.edit_text_duration)
+    EditText mEtDuration;
 
+    WeakReference<EditText> mCurrentEditTextWeakRef;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_bezier_interpolator);
+        ButterKnife.bind(this);
+
+        mSbValueEdit.setMax(1000);
+
+        mEtC1x.setOnFocusChangeListener(mCoordinateFocusChangeListener);
+        mEtC1y.setOnFocusChangeListener(mCoordinateFocusChangeListener);
+        mEtC2x.setOnFocusChangeListener(mCoordinateFocusChangeListener);
+        mEtC2y.setOnFocusChangeListener(mCoordinateFocusChangeListener);
+        mEtDuration.setOnFocusChangeListener(mDurationFocusChangeListener);
+
+        mSbValueEdit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (mCurrentEditTextWeakRef != null && mCurrentEditTextWeakRef.get() != null) {
+                    EditText editText = mCurrentEditTextWeakRef.get();
+                    if (editText == mEtDuration) {
+                        editText.setText(String.format(Locale.getDefault(), "%d", i));
+                    } else {
+                        editText.setText(String.format(Locale.getDefault(), "%.2f", (((float)i) / 1000.f)));
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+
+    private View.OnFocusChangeListener mCoordinateFocusChangeListener = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View view, boolean b) {
+            if (!(view instanceof EditText)) {
+                return;
+            }
+            EditText editText = (EditText) view;
+            try {
+                float value = Float.parseFloat(editText.getText().toString());
+                mSbValueEdit.setProgress((int)(value * 1000));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                return;
+            }
+            mCurrentEditTextWeakRef = new WeakReference<>(editText);
+        }
+    };
+
+    private View.OnFocusChangeListener mDurationFocusChangeListener = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View view, boolean b) {
+            if (!(view instanceof EditText)) {
+                return;
+            }
+            EditText editText = (EditText) view;
+            try {
+                int value = Integer.parseInt(editText.getText().toString());
+                mSbValueEdit.setProgress(value);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                return;
+            }
+            mCurrentEditTextWeakRef = new WeakReference<>(editText);
+        }
+    };
+
 
 
     /*
