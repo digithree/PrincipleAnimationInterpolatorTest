ad9a2f946e3a260223f1a3dd3af25d6a66b46f76
Simon Kenny <digithree@gmail.com>
2016-09-07 11:26:55 +0100

    Add empty activity for bezier and spring interpolator edit, with layout and launched by main menu button click

--- app/src/main/java/ie/simonkenny/principleanimationinterpolatortest/MainMenuActivity.java
+++ app/src/main/java/ie/simonkenny/principleanimationinterpolatortest/MainMenuActivity.java
@@ -1,8 +1,12 @@
 package ie.simonkenny.principleanimationinterpolatortest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by simonkenny on 07/09/2016.
@@ -14,5 +18,17 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.thumb_button_bezier)
+    public void onBezierThumbButtonClick() {
+        startActivity(new Intent(this, BezierInterpolatorActivity.class));
+    }
+
+    @OnClick(R.id.thumb_button_spring)
+    public void onSpringThumbButtonClick() {
+        startActivity(new Intent(this, SpringInterpolatorActivity.class));
     }
 }
